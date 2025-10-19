2b742710e2b686f6d6129675f26129c39c7c3a72
Okan Gezmen <96254826+ogezmen@users.noreply.github.com>
2025-10-19 13:28:03 +0200

    Fix compile and run script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-project_path=${0%/*}
-javac -d $project_path/out/production $(find $project_path/src -name "*.java")
+javac -source 25 -d out/production $(find src -name "*.java")
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-project_path=${0%/*}
-cd $project_path/out/production; java OrderSystemMain
+cd out/production; java OrderSystemMain
