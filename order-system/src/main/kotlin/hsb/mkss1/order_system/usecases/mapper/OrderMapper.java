9696c05e5d4bb0abb9bf586b934fc0801bf2d28a
Okan Gezmen <okan.gezmen@outlook.de>
2025-12-10 18:28:40 +0100

    Fix compile error

--- order-system/src/main/kotlin/hsb/mkss1/order_system/usecases/mapper/OrderMapper.java
+++ order-system/src/main/kotlin/hsb/mkss1/order_system/usecases/mapper/OrderMapper.java
@@ -1,8 +1,7 @@
 package hsb.mkss1.order_system.usecases.mapper;
 
-import hsb.mkss1.order_system.entities.Item;
 import hsb.mkss1.order_system.entities.Order;
-import hsb.mkss1.order_system.usecases.dtos.ItemDto;
+import hsb.mkss1.order_system.entities.OrderStatusEnum;
 import hsb.mkss1.order_system.usecases.dtos.OrderDto;
 
 import java.util.ArrayList;
@@ -27,6 +26,7 @@ public class OrderMapper {
 
     public static Order mapDtoToEntity(OrderDto dto) {
         return new Order(dto.getId(),
+                OrderStatusEnum.EMPTY,
                 new ArrayList<>(),
                 dto.getCheckoutTimestamp(),
                 dto.getLumpSum(),
